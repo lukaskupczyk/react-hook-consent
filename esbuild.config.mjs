80c58b4f0e195e2350449f467c6fbe2d5e580c3b
Lukas Kupczyk <lukas@kupczyk.net>
2022-12-28 18:46:10 +0100

    Add dev script

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,15 +1,27 @@
 import { sassPlugin } from 'esbuild-sass-plugin';
 import esbuild from 'esbuild';
 
-esbuild
-    .build({
-        entryPoints: ['src/styles/style.scss', 'src/index.ts'],
-        bundle: true,
-        minify: true,
-        format: 'cjs',
-        sourcemap: true,
-        outdir: './dist',
-        external: ['react', 'react-dom'],
-        plugins: [sassPlugin()],
-    })
-    .catch(() => process.exit(1));
+const base = {
+    entryPoints: ['src/styles/style.scss', 'src/index.ts'],
+    bundle: true,
+    minify: true,
+    format: 'cjs',
+    sourcemap: true,
+    outdir: './dist',
+    external: ['react', 'react-dom'],
+    plugins: [sassPlugin()],
+};
+
+const modes = {
+    production: {},
+    dev: { watch: true },
+};
+
+const args = process.argv.slice(2);
+const mode = args[0];
+
+if (!(mode in modes)) {
+    throw new Error('mode not set');
+}
+
+esbuild.build({ ...base, ...modes[mode] }).catch(() => process.exit(1));
