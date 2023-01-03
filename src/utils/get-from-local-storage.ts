6dc4b3b10902568a7270db8089ed08ce1b1a6fbe
Lukas <lukaskupczyk@users.noreply.github.com>
2023-01-03 16:11:31 +0100

    Add hash to monitor change of options (#2)

---
+++ src/utils/get-from-local-storage.ts
@@ -0,0 +1,21 @@
+import { localStorageKey } from './config';
+import { Consent } from '../Context';
+
+type StoredState = { consent?: Consent; hash?: string };
+
+export function getFromLocalStorage(hash: string) {
+    const item = localStorage.getItem(`${localStorageKey}`);
+
+    if (!item) {
+        return { consent: [], isBannerVisible: true };
+    }
+
+    const { consent, hash: storedHash } = JSON.parse(item) as StoredState;
+
+    let isBannerVisible = false;
+    if (storedHash !== hash) {
+        isBannerVisible = true;
+    }
+
+    return { consent: consent && consent.length > 0 ? consent : [], isBannerVisible };
+}
