a70f12951e7a2d6e232fceb1ef1fd571396ea6b8
Lukas Kupczyk <lukas@kupczyk.net>
2023-01-03 16:09:06 +0100

    Add hash to monitor changed options

--- src/useConsentState.tsx
+++ src/useConsentState.tsx
@@ -1,17 +1,19 @@
-import { useEffect, useCallback, useState } from 'react';
+import hash from 'object-hash';
+import { useCallback, useEffect, useState } from 'react';
 import { Consent, ConsentOptions } from './Context';
 import { addScripts } from './scripts/add';
-import { localStorageKey } from './utils/config';
 import { getFromLocalStorage } from './utils/get-from-local-storage';
+import { saveToLocalStorage } from './utils/save-to-local-storage';
 
 export function useConsentState(options: ConsentOptions) {
-    const [state, setState] = useState<{ consent: Consent; isBannerVisible: boolean }>({
+    const [state, setState] = useState<{ consent: Consent; isBannerVisible: boolean; hash: string }>({
         consent: [],
         isBannerVisible: false,
+        hash: hash(options),
     });
 
     useEffect(() => {
-        const { consent, isBannerVisible } = getFromLocalStorage();
+        const { consent, isBannerVisible } = getFromLocalStorage(state.hash);
 
         options.services.forEach(({ id, scripts }) => {
             if (consent.includes(id)) {
@@ -19,13 +21,13 @@ export function useConsentState(options: ConsentOptions) {
             }
         });
 
-        setState({ consent, isBannerVisible });
+        setState((state) => ({ ...state, consent, isBannerVisible }));
     }, [options.services]);
 
-    const setConsent = useCallback((consent: string[]) => {
-        setState({ consent, isBannerVisible: false });
+    const setConsent = useCallback((consent: Consent) => {
+        setState((state) => ({ ...state, consent, isBannerVisible: false }));
 
-        localStorage.setItem(`${localStorageKey}`, JSON.stringify({ consent }));
+        saveToLocalStorage(consent, state.hash);
     }, []);
 
     const toggleBanner = useCallback(() => {
