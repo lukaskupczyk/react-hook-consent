0546e1fe9f68fe45a9cd0e8d33de5adc0aaf890f
Lukas Kupczyk <lukas@kupczyk.net>
2023-01-10 22:51:38 +0100

    Update readme

--- examples/next-v13/pages/_app.tsx
+++ examples/next-v13/pages/_app.tsx
@@ -7,8 +7,20 @@ import 'react-hook-consent/dist/styles/style.css';
 const consentOptions: ConsentOptions = {
     services: [
         {
-            id: 'myid',
-            name: 'MyName',
+            id: 'myid_1',
+            name: 'My Ads',
+            description:
+                'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+            scripts: [
+                { id: 'external-script', src: 'https://myscript.com/script.js' },
+                { id: 'inline-code', code: `alert("I am a JavaScript code");` },
+            ],
+        },
+        {
+            id: 'myid_2',
+            name: 'My Maps',
+            description:
+                'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
             scripts: [
                 { id: 'external-script', src: 'https://myscript.com/script.js' },
                 { id: 'inline-code', code: `alert("I am a JavaScript code");` },
@@ -21,11 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <ConsentProvider options={consentOptions}>
             <Component {...pageProps} />
-            <ConsentBanner>
-                <>
-                    Optional individual consent text including a <Link href={'/policy'}>link</Link>
-                </>
-            </ConsentBanner>
+            <ConsentBanner />
         </ConsentProvider>
     );
 }
