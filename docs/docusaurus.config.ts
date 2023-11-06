b3c45edbaf9ef3a8902739f8a84fde6989454091
Lukas Kupczyk <lukas@kupczyk.net>
2023-11-06 21:50:48 +0100

    Add docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,12 +1,12 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 const config: Config = {
     title: 'React Hook Consent',
     tagline:
         'React consent management solution and banner for cookies, local storage, session storage and (external) scripts.',
-    favicon: 'img/favicon.ico',
+    // favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
     url: 'https://lukaskupczyk.github.io',
@@ -40,18 +40,6 @@ const config: Config = {
                     sidebarPath: './sidebars.ts',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                theme: {
-                    customCss: './src/css/custom.css',
                 },
             } satisfies Preset.Options,
         ],
@@ -61,70 +49,26 @@ const config: Config = {
         // Replace with your project's social card
         image: 'img/docusaurus-social-card.jpg',
         navbar: {
-            title: 'My Site',
-            logo: {
-                alt: 'My Site Logo',
-                src: 'img/logo.svg',
-            },
+            title: 'React Hook Consent',
             items: [
                 {
                     type: 'docSidebar',
-                    sidebarId: 'tutorialSidebar',
+                    sidebarId: 'gettingStartedSidebar',
                     position: 'left',
-                    label: 'Tutorial',
+                    label: 'Getting Started',
                 },
-                { to: '/blog', label: 'Blog', position: 'left' },
                 {
-                    href: 'https://github.com/facebook/docusaurus',
+                    type: 'docSidebar',
+                    sidebarId: 'apiSidebar',
+                    position: 'left',
+                    label: 'API',
+                },
+                {
+                    href: 'https://github.com/lukaskupczyk/react-hook-consent',
                     label: 'GitHub',
                     position: 'right',
                 },
             ],
-        },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Docs',
-                    items: [
-                        {
-                            label: 'Tutorial',
-                            to: '/docs/intro',
-                        },
-                    ],
-                },
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'Stack Overflow',
-                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discordapp.com/invite/docusaurus',
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/docusaurus',
-                        },
-                    ],
-                },
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog',
-                        },
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/facebook/docusaurus',
-                        },
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
         },
         prism: {
             theme: prismThemes.github,
